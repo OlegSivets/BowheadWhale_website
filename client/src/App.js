6609c04745c840bdea7de0c8396e9a4945c2ecab
legol <legolas2002@mail.ru>
2022-11-04 14:45:56 +0300

    Drag and Drop zaone test

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [drag, setDrag] = useState(false);
+
+  useEffect(() => {
+    fetch('/data')
+      .then(response => response.json())
+      .then(res => {
+        console.log('test data fetching', res);
+        setData(res);
+      });
+  }, []);
+
+  function dragHandler(e, state) {
+    e.preventDefault(e);
+    setDrag(state);
+  }
+
+  function onDropHandler(e) {
+    e.preventDefault(e);
+    let files = [...e.dataTransfer.files];
+    console.log(files);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        test data fetching
+        {typeof data.elems === 'undefined' ? (
+          <p>Loading...</p>
+        ) : (
+          data.elems.map((elem, i) => <p key={i}>{elem}</p>)
+        )}
+      </div>
+      <div>
+        {drag ? (
+          <div
+            className='drop-area'
+            onDragStart={e => dragHandler(e, true)}
+            onDragLeave={e => dragHandler(e, false)}
+            onDragOver={e => dragHandler(e, true)}
+            onDrop={e => onDropHandler(e)}>
+            Отпустите файлы, чтобы загрузить
+          </div>
+        ) : (
+          <div
+            className='drop-area'
+            onDragStart={e => dragHandler(e, true)}
+            onDragLeave={e => dragHandler(e, false)}
+            onDragOver={e => dragHandler(e, true)}>
+            Перетащите файлы в область загрузки
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
