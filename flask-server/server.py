fa1a3d5ffd31a682ce1f6a9cf24d3131cd54f4f2
Mikhail Kozlov <33799845+Borntowarn@users.noreply.github.com>
2022-11-06 16:40:09 +0300

    Загрузка и обработка архивов Возврат эндпоинтов загруженных изображений

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,35 +1,50 @@
-from flask import *
 import os
-import matplotlib.pyplot as plt
+import patoolib
+from werkzeug.utils import secure_filename
+from flask import Flask, request, make_response, jsonify, send_file
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = set(['jp2', 'raw', 'png', 'jpg', 'jpeg', 'zip'])
+
+UPLOAD_FOLDER = './uploaded'
+ALLOWED_EXTENSIONS = set(['jp2', 'raw', 'png', 'jpg', 'jpeg', 'zip', 'rar', 'gz', '7z'])
 
 app = Flask(__name__)
-
-UPLOAD_FOLDER = '/uploaded'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/data')
-def data():
-    return {
-        "elems": ['data1', 'data2']
-    }
+
+def allowed_file(filename):
+    """Проверка допустимых типов загружаемых файлов"""
+    ext = filename.rsplit('.', 1)[1]
+    return ext if '.' in filename and ext in ALLOWED_EXTENSIONS else False
+
+
+@app.route('/<name>')
+def uploaded_images(name):
+    """Возврат загруженных изображений клиенту"""
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], name))
 
 
 @app.route('/files', methods=['POST'])
-def getClientFiles():
-    getData = request.files
-    for i in range(len(getData)):
-        f = getData[f'file_{i}']
-        f.save('./uploaded/' + f.filename)
-        #return redirect(url_for('uploaded_file', filename=filee.filename))
-    res = make_response(jsonify({"message": "files were received"}), 200)
+def get_client_files():
+    """Получение файлов от клиента и их обработка"""
+    files = request.files
+    for i in range(len(files)):
+        f = files[f'file_{i}']
+        f_ext = allowed_file(f.filename)
+        if f and f_ext:
+            filename = secure_filename(f.filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            n = 1
+            while os.path.isfile(path):
+                parts = path.rsplit('.', 1)
+                parts[0] += f'_{str(n)}'
+                path = '.'.join(parts)
+                n += 1
+            f.save(path)
+            if f_ext == 'zip' or f_ext == 'gz' or f_ext == 'rar' or f_ext == '7z':
+                patoolib.extract_archive(path, outdir=app.config['UPLOAD_FOLDER'])
+                os.remove(path)
+    res = make_response(jsonify(os.listdir(app.config['UPLOAD_FOLDER'])), 200)
     return res
-
-# @app.route('/result', methods=['POST'])
-# def showResultFiles():
-#     plt.imshow()
 
 if __name__ == '__main__':
     app.run(debug=True)
