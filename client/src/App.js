ad5b0738d4d98910327d69342c9376857dbc1bf6
legol <legolas2002@mail.ru>
2022-11-11 23:19:49 +0300

    file directories were added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
 import DragArea from './components/Areas/DragArea';
-import FilesUploadButton from './components/Buttons/FilesUploadButton';
 import MainContextProvider from './contex/MainContext';
 import HeaderArea from './components/Areas/header/HeaderArea';
 import UploadedFilesArea from './components/Areas/UploadedFilesArea';
 import UploadLabelArea from './components/Areas/UploadLabelArea';
-import DownloadButton from './components/Buttons/DownloadButton';
-import FilesDeleteButton from './components/Buttons/FilesDeleteButton';
 import UploadManageArea from './components/Areas/UploadManageArea';
 
 function App() {
